517819b068788b3a4a36182550a8487637ffc977
Darkness Hidden(TEA) <kmd36rk@gmail.com>
2021-11-30 03:07:43 +0700

    Update fmr_core.cpp

--- plugins/MediatekRadio/fmr_core.cpp
+++ plugins/MediatekRadio/fmr_core.cpp
@@ -58,24 +58,24 @@ int FMR_get_cfgs(int idx)
 
     FMR_cust_hdler(idx) = dlopen(CUST_LIB_NAME, RTLD_NOW);
     if (FMR_cust_hdler(idx) == NULL) {
-        LOGE("%s failed, %s\n", __FUNCTION__, dlerror());
+     //   LOGE("%s failed, %s\n", __FUNCTION__, dlerror());
         //FMR_seterr(ERR_LD_LIB);
     } else {
         *(void **) (&FMR_get_cfg(idx)) = dlsym(FMR_cust_hdler(idx), "CUST_get_cfg");
         if (FMR_get_cfg(idx) == NULL) {
-            LOGE("%s failed, %s\n", __FUNCTION__, dlerror());
+        //    LOGE("%s failed, %s\n", __FUNCTION__, dlerror());
             //FMR_seterr(ERR_FIND_CUST_FNUC);
         } else {
-            LOGI("Go to run cust function\n");
+        //    LOGI("Go to run cust function\n");
             (*FMR_get_cfg(idx))(&(pfmr_data[idx]->cfg_data));
-            LOGI("OK\n");
+       //     LOGI("OK\n");
             ret = 0;
         }
         //dlclose(FMR_cust_hdler(idx));
         FMR_cust_hdler(idx) = NULL;
         FMR_get_cfg(idx) = NULL;
     }
-    LOGI("%s successfully. chip: 0x%x, lband: %d, hband: %d, seek_space: %d, max_scan_num: %d\n", __FUNCTION__, FMR_chip(idx), FMR_low_band(idx), FMR_high_band(idx), FMR_seek_space(idx), FMR_max_scan_num(idx));
+   // LOGI("%s successfully. chip: 0x%x, lband: %d, hband: %d, seek_space: %d, max_scan_num: %d\n", __FUNCTION__, FMR_chip(idx), FMR_low_band(idx), FMR_high_band(idx), FMR_seek_space(idx), FMR_max_scan_num(idx));
 
     return ret;
 }
@@ -103,7 +103,7 @@ int FMR_init()
             break;
         }
     }
-    LOGI("FMR idx = %d\n", idx);
+    //LOGI("FMR idx = %d\n", idx);
     if (idx == FMR_MAX_IDX) {
         //FMR_seterr(ERR_NO_MORE_IDX);
         return -1;
@@ -115,23 +115,23 @@ int FMR_init()
     memset(pfmr_data[idx], 0, sizeof(struct fmr_ds));
 
     if (FMR_get_cfgs(idx) < 0) {
-        LOGI("FMR_get_cfgs failed\n");
+     //   LOGI("FMR_get_cfgs failed\n");
         goto fail;
     }
 
     if (FMR_chk_cfg_data(idx) < 0) {
-        LOGI("FMR_chk_cfg_data failed\n");
+     //   LOGI("FMR_chk_cfg_data failed\n");
         goto fail;
     }
 
     pfmr_data[idx]->init_func = FM_interface_init;
     if (pfmr_data[idx]->init_func == NULL) {
-        LOGE("%s init_func error, %s\n", __func__, dlerror());
+      //  LOGE("%s init_func error, %s\n", __func__, dlerror());
         goto fail;
     } else {
-        LOGI("Go to run init function\n");
+   //     LOGI("Go to run init function\n");
         (*pfmr_data[idx]->init_func)(&(pfmr_data[idx]->tbl));
-        LOGI("OK\n");
+     //   LOGI("OK\n");
         ret = 0;
     }
 
@@ -152,19 +152,19 @@ int FMR_open_dev(int idx)
 
     ret = FMR_cbk_tbl(idx).open_dev(FM_DEV_NAME, &FMR_fd(idx));
     if (ret || FMR_fd(idx) < 0) {
-        LOGE("%s failed, [fd=%d]\n", __func__, FMR_fd(idx));
+     //   LOGE("%s failed, [fd=%d]\n", __func__, FMR_fd(idx));
         return ret;
     }
 
     //Check if customer's cfg matchs driver.
     ret = FMR_get_chip_id(idx, &real_chip);
     if (FMR_chip(idx) != real_chip) {
-        LOGE("%s, Chip config error. 0x%x\n", __func__, real_chip);
+      //  LOGE("%s, Chip config error. 0x%x\n", __func__, real_chip);
         ret = FMR_cbk_tbl(idx).close_dev(FMR_fd(idx));
         return ret;
     }
 
-    LOGD("%s, [fd=%d] [chipid=0x%x] [ret=%d]\n", __func__, FMR_fd(idx), real_chip, ret);
+   // LOGD("%s, [fd=%d] [chipid=0x%x] [ret=%d]\n", __func__, FMR_fd(idx), real_chip, ret);
     return ret;
 }
 
@@ -174,7 +174,7 @@ int FMR_close_dev(int idx)
 
     FMR_ASSERT(FMR_cbk_tbl(idx).close_dev);
     ret = FMR_cbk_tbl(idx).close_dev(FMR_fd(idx));
-    LOGD("%s, [fd=%d] [ret=%d]\n", __func__, FMR_fd(idx), ret);
+    //LOGD("%s, [fd=%d] [ret=%d]\n", __func__, FMR_fd(idx), ret);
     return ret;
 }
 
@@ -184,18 +184,18 @@ int FMR_pwr_up(int idx, int freq)
 
     FMR_ASSERT(FMR_cbk_tbl(idx).pwr_up);
 
-    LOGI("%s,[freq=%d]\n", __func__, freq);
+   // LOGI("%s,[freq=%d]\n", __func__, freq);
     if (freq < fmr_data.cfg_data.low_band || freq > fmr_data.cfg_data.high_band) {
-        LOGE("%s error freq: %d\n", __func__, freq);
+    //    LOGE("%s error freq: %d\n", __func__, freq);
         ret = -ERR_INVALID_PARA;
         return ret;
     }
     ret = FMR_cbk_tbl(idx).pwr_up(FMR_fd(idx), fmr_data.cfg_data.band, freq);
     if (ret) {
-        LOGE("%s failed, [ret=%d]\n", __func__, ret);
+      //  LOGE("%s failed, [ret=%d]\n", __func__, ret);
     }
     fmr_data.cur_freq = freq;
-    LOGD("%s, [ret=%d]\n", __func__, ret);
+   // LOGD("%s, [ret=%d]\n", __func__, ret);
     return ret;
 }
 
@@ -205,7 +205,7 @@ int FMR_pwr_down(int idx, int type)
 
     FMR_ASSERT(FMR_cbk_tbl(idx).pwr_down);
     ret = FMR_cbk_tbl(idx).pwr_down(FMR_fd(idx), type);
-    LOGD("%s, [ret=%d]\n", __func__, ret);
+  //  LOGD("%s, [ret=%d]\n", __func__, ret);
     return ret;
 }
 
@@ -218,10 +218,10 @@ int FMR_get_chip_id(int idx, int *chipid)
 
     ret = FMR_cbk_tbl(idx).get_chip_id(FMR_fd(idx), chipid);
     if (ret) {
-        LOGE("%s failed, %s\n", __func__, FMR_strerr());
+   //     LOGE("%s failed, %s\n", __func__, FMR_strerr());
         *chipid = -1;
     }
-    LOGD("%s, [ret=%d]\n", __func__, ret);
+  //  LOGD("%s, [ret=%d]\n", __func__, ret);
     return ret;
 }
 
@@ -233,7 +233,7 @@ int FMR_get_ps(int idx, uint8_t **ps, int *ps_len)
     FMR_ASSERT(ps);
     FMR_ASSERT(ps_len);
     ret = FMR_cbk_tbl(idx).get_ps(FMR_fd(idx), &fmr_data.rds, ps, ps_len);
-    LOGD("%s, [ret=%d]\n", __func__, ret);
+   // LOGD("%s, [ret=%d]\n", __func__, ret);
     return ret;
 }
 
@@ -246,7 +246,7 @@ int FMR_get_rt(int idx, uint8_t **rt, int *rt_len)
     FMR_ASSERT(rt_len);
 
     ret = FMR_cbk_tbl(idx).get_rt(FMR_fd(idx), &fmr_data.rds, rt, rt_len);
-    LOGD("%s, [ret=%d]\n", __func__, ret);
+   // LOGD("%s, [ret=%d]\n", __func__, ret);
     return ret;
 }
 
@@ -258,10 +258,10 @@ int FMR_tune(int idx, int freq)
 
     ret = FMR_cbk_tbl(idx).tune(FMR_fd(idx), freq, fmr_data.cfg_data.band);
     if (ret) {
-        LOGE("%s failed, [ret=%d]\n", __func__, ret);
+   //     LOGE("%s failed, [ret=%d]\n", __func__, ret);
     }
     fmr_data.cur_freq = freq;
-    LOGD("%s, [freq=%d] [ret=%d]\n", __func__, freq, ret);
+   // LOGD("%s, [freq=%d] [ret=%d]\n", __func__, freq, ret);
     return ret;
 }
 
@@ -288,7 +288,7 @@ fm_bool FMR_SevereDensense(fm_u16 ChannelNo, fm_s32 RSSI)
         if (ChannelNo == chan_info->chan[i].freq) {
             //if (RSSI < FM_SEVERE_RSSI_TH)
             if (RSSI < chan_info->chan[i].rssi_th) {
-                LOGI(" SevereDensense[%d] RSSI[%d]\n", ChannelNo,RSSI);
+          //      LOGI(" SevereDensense[%d] RSSI[%d]\n", ChannelNo,RSSI);
                 return fm_true;
             } else {
                 break;
@@ -327,27 +327,27 @@ static fm_bool FMR_Seek_TuneCheck(int idx, fm_softmute_tune_t *cur_freq)
     int ret = 0;
     if (fmr_data.scan_stop == fm_true) {
         ret = FMR_tune(idx,fmr_data.cur_freq);
-        LOGI("seek stop!!! tune ret=%d",ret);
+     //   LOGI("seek stop!!! tune ret=%d",ret);
         return fm_false;
     }
     ret = FMR_cbk_tbl(idx).soft_mute_tune(FMR_fd(idx), cur_freq);
     if (ret) {
-        LOGE("soft mute tune, failed:[%d]\n",ret);
+    //    LOGE("soft mute tune, failed:[%d]\n",ret);
         cur_freq->valid = fm_false;
         return fm_true;
     }
     if (cur_freq->valid == fm_true)/*get valid channel*/ {
         if (FMR_DensenseDetect(idx, cur_freq->freq, cur_freq->rssi) == fm_true) {
-            LOGI("desense channel detected:[%d] \n", cur_freq->freq);
+      //      LOGI("desense channel detected:[%d] \n", cur_freq->freq);
             cur_freq->valid = fm_false;
             return fm_true;
         }
         if (FMR_SevereDensense(cur_freq->freq, cur_freq->rssi) == fm_true) {
-            LOGI("sever desense channel detected:[%d] \n", cur_freq->freq);
+       //     LOGI("sever desense channel detected:[%d] \n", cur_freq->freq);
             cur_freq->valid = fm_false;
             return fm_true;
         }
-        LOGI("seek result freq:[%d] \n", cur_freq->freq);
+      //  LOGI("seek result freq:[%d] \n", cur_freq->freq);
     }
     return fm_true;
 }
@@ -377,7 +377,7 @@ static int FMR_Seek_More(int idx, fm_softmute_tune_t *validfreq, fm_u8 seek_dir,
                 if (cur_freq.rssi > validfreq->rssi) {
                     validfreq->freq = cur_freq.freq;
                     validfreq->rssi = cur_freq.rssi;
-                    LOGI("seek cover last by freq=%d",cur_freq.freq);
+      //              LOGI("seek cover last by freq=%d",cur_freq.freq);
                 }
             }
         } else {
@@ -399,7 +399,7 @@ int FMR_seek_Channel(int idx, int start_freq, int min_freq, int max_freq, int ba
     if (dir == 1)/*forward*/ {
         for (i=((start_freq-min_freq)/seek_space+1); i<band_channel_no; i++) {
             cur_freq.freq = min_freq + seek_space*i;
-            LOGI("i=%d, freq=%d-----1",i,cur_freq.freq);
+    //        LOGI("i=%d, freq=%d-----1",i,cur_freq.freq);
             ret = FMR_Seek_TuneCheck(idx, &cur_freq);
             if (ret == fm_false) {
                 return -1;
@@ -419,7 +419,7 @@ int FMR_seek_Channel(int idx, int start_freq, int min_freq, int max_freq, int ba
         }
         for (i=0; i<((start_freq-min_freq)/seek_space); i++) {
             cur_freq.freq = min_freq + seek_space*i;
-            LOGI("i=%d, freq=%d-----2",i,cur_freq.freq);
+       //     LOGI("i=%d, freq=%d-----2",i,cur_freq.freq);
             ret = FMR_Seek_TuneCheck(idx, &cur_freq);
             if (ret == fm_false) {
                 return -1;
@@ -440,7 +440,7 @@ int FMR_seek_Channel(int idx, int start_freq, int min_freq, int max_freq, int ba
     } else/*backward*/ {
         for (i=((start_freq-min_freq)/seek_space-1); i>=0; i--) {
             cur_freq.freq = min_freq + seek_space*i;
-            LOGI("i=%d, freq=%d-----3",i,cur_freq.freq);
+       //     LOGI("i=%d, freq=%d-----3",i,cur_freq.freq);
             ret = FMR_Seek_TuneCheck(idx, &cur_freq);
             if (ret == fm_false) {
                 return -1;
@@ -460,7 +460,7 @@ int FMR_seek_Channel(int idx, int start_freq, int min_freq, int max_freq, int ba
         }
         for (i=(band_channel_no-1); i>((start_freq-min_freq)/seek_space); i--) {
             cur_freq.freq = min_freq + seek_space*i;
-            LOGI("i=%d, freq=%d-----4",i,cur_freq.freq);
+       //     LOGI("i=%d, freq=%d-----4",i,cur_freq.freq);
             ret = FMR_Seek_TuneCheck(idx, &cur_freq);
             if (ret == fm_false) {
                 return -1;
@@ -494,7 +494,7 @@ int FMR_seek(int idx, int start_freq, int dir, int *ret_freq)
     int rssi;
 
     if ((start_freq < 7600) || (start_freq > 10800)) /*need replace by macro*/ {
-        LOGE("%s error start_freq: %d\n", __func__, start_freq);
+       // LOGE("%s error start_freq: %d\n", __func__, start_freq);
         return -ERR_INVALID_PARA;
     }
 
@@ -521,7 +521,7 @@ int FMR_seek(int idx, int start_freq, int dir, int *ret_freq)
     }
 
     fmr_data.scan_stop = fm_false;
-    LOGD("seek start freq %d band_channel_no=[%d], seek_space=%d band[%d - %d] dir=%d\n", start_freq, band_channel_no,seek_space,min_freq,max_freq,dir);
+   // LOGD("seek start freq %d band_channel_no=[%d], seek_space=%d band[%d - %d] dir=%d\n", start_freq, band_channel_no,seek_space,min_freq,max_freq,dir);
 
     ret = FMR_seek_Channel(idx, start_freq, min_freq, max_freq, band_channel_no, seek_space, dir, ret_freq, &rssi);
 
@@ -535,14 +535,14 @@ int FMR_set_mute(int idx, int mute)
     FMR_ASSERT(FMR_cbk_tbl(idx).set_mute)
 
     if ((mute < 0) || (mute > 1)) {
-        LOGE("%s error param mute:  %d\n", __func__, mute);
+   //     LOGE("%s error param mute:  %d\n", __func__, mute);
     }
 
     ret = FMR_cbk_tbl(idx).set_mute(FMR_fd(idx), mute);
     if (ret) {
-        LOGE("%s failed, %s\n", __func__, FMR_strerr());
+    //    LOGE("%s failed, %s\n", __func__, FMR_strerr());
     }
-    LOGD("%s, [mute=%d] [ret=%d]\n", __func__, mute, ret);
+    // LOGD("%s, [mute=%d] [ret=%d]\n", __func__, mute, ret);
     return ret;
 }
 
@@ -556,9 +556,9 @@ int FMR_is_rdsrx_support(int idx, int *supt)
     ret = FMR_cbk_tbl(idx).is_rdsrx_support(FMR_fd(idx), supt);
     if (ret) {
         *supt = 0;
-        LOGE("%s, failed\n", __func__);
+     //   LOGE("%s, failed\n", __func__);
     }
-    LOGD("%s, [supt=%d] [ret=%d]\n", __func__, *supt, ret);
+    // LOGD("%s, [supt=%d] [ret=%d]\n", __func__, *supt, ret);
     return ret;
 }
 
@@ -595,18 +595,18 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
 #endif
 
     memset(SortData, 0, CQI_CH_NUM_MAX*sizeof(struct fm_cqi));
-    LOGI("band_channel_no=[%d], seek_space=%d, start freq=%d\n", band_channel_no,seek_space,Start_Freq);
+   // LOGI("band_channel_no=[%d], seek_space=%d, start freq=%d\n", band_channel_no,seek_space,Start_Freq);
     for (i=0; i<band_channel_no; i++) {
         if (fmr_data.scan_stop == fm_true) {
             FMR_Restore_Search(idx);
             ret = FMR_tune(idx, fmr_data.cur_freq);
-            LOGI("scan stop!!! tune ret=%d",ret);
+       //     LOGI("scan stop!!! tune ret=%d",ret);
             return -1;
         }
         cur_freq.freq = Start_Freq + seek_space*i;
         ret = FMR_cbk_tbl(idx).soft_mute_tune(FMR_fd(idx), &cur_freq);
         if (ret) {
-            LOGE("soft mute tune, failed:[%d]\n",ret);
+        //    LOGE("soft mute tune, failed:[%d]\n",ret);
             LastExist = fm_false;
             continue;
         }
@@ -615,7 +615,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
             memset(F, fm_false, sizeof(F));
 #endif
             if (FMR_DensenseDetect(idx, cur_freq.freq, cur_freq.rssi) == fm_true) {
-                LOGI("desense channel detected:[%d] \n", cur_freq.freq);
+         //       LOGI("desense channel detected:[%d] \n", cur_freq.freq);
                 LastExist = fm_false;
                 continue;
             }
@@ -628,7 +628,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
                     SortData[Num-1].ch=cur_freq.freq;
                     SortData[Num-1].rssi=cur_freq.rssi;
                     SortData[Num-1].reserve = 1;
-                    LOGI("cover last channel \n");
+         //           LOGI("cover last channel \n");
                 } else/*ignore current freq*/ {
                     LastExist = fm_false;
                     continue;
@@ -643,7 +643,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
                 SortData[Num].reserve = 1;
                 Num++;
                 LastExist = fm_true;
-                LOGI("Num++:[%d] \n", Num);
+           //     LOGI("Num++:[%d] \n", Num);
             }
         } else {
 #if (FMR_NOISE_FLOORT_DETECT==1)
@@ -658,7 +658,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
                     F_Rssi[1] = F_Rssi[2];
                     F[2] = fm_false;
                     F_Rssi[2] = 0;
-                    LOGI("FM Noise FLoor:Pacc=[%d] Nacc=[%d] \n", Pacc,Nacc);
+          //          LOGI("FM Noise FLoor:Pacc=[%d] Nacc=[%d] \n", Pacc,Nacc);
                 }
             } else {
                 memset(F, fm_false, sizeof(F));
@@ -677,12 +677,12 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
             Nacc = 0;
             for (j=NF_Idx; j<Num; j++) {
                 if (SortData[j].rssi < (NF+FM_NOISE_FLOOR_OFFSET)) {
-                    LOGI("FM Noise FLoor Detected:freq=[%d] NF=[%d] \n", SortData[j].ch,NF);
+          //          LOGI("FM Noise FLoor Detected:freq=[%d] NF=[%d] \n", SortData[j].ch,NF);
                     SortData[j].reserve = 0;
                 }
             }
             NF_Idx = j;
-            LOGI("FM Noise FLoor NF_Idx[%d] \n", NF_Idx);
+         //   LOGI("FM Noise FLoor NF_Idx[%d] \n", NF_Idx);
         }
 #endif
     }
@@ -693,7 +693,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
         return -1;
     }
     for (i=0; i<Num; i++)/*debug*/ {
-        LOGI("[%d]:%d \n", i,SortData[i].ch);
+      //  LOGI("[%d]:%d \n", i,SortData[i].ch);
     }
 
     switch (fmr_data.cfg_data.scan_sort)
@@ -701,7 +701,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
         case FM_SCAN_SORT_UP:
         case FM_SCAN_SORT_DOWN:
             {
-                LOGI("Start sort \n");
+        //        LOGI("Start sort \n");
                 //do sort: insert sort algorithm
                 for (i = 1; i < Num; i++) {
                     for (j = i; (j > 0) && ((FM_SCAN_SORT_DOWN == fmr_data.cfg_data.scan_sort) ? (SortData[j-1].rssi \
@@ -711,7 +711,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
                         memcpy(&SortData[j-1], &swap, sizeof(struct fm_cqi));
                     }
                 }
-                LOGI("End sort \n");
+         //       LOGI("End sort \n");
                 break;
             }
         default:
@@ -729,7 +729,7 @@ int FMR_scan_Channels(int idx, uint16_t *scan_tbl, int *max_cnt, fm_s32 band_cha
     }
     *max_cnt=ChannelNo;
 
-    LOGI("return channel no.[%d] \n", ChannelNo);
+   // LOGI("return channel no.[%d] \n", ChannelNo);
     return 0;
 }
 
@@ -783,7 +783,7 @@ int FMR_turn_on_off_rds(int idx, int onoff)
     if (ret) {
         LOGE("%s, failed\n", __func__);
     }
-    LOGD("%s, [onoff=%d] [ret=%d]\n", __func__, onoff, ret);
+   // LOGD("%s, [onoff=%d] [ret=%d]\n", __func__, onoff, ret);
     return ret;
 }
 
@@ -798,7 +798,7 @@ int FMR_read_rds_data(int idx, uint16_t *rds_status)
     /*if (ret) {
         LOGE("%s, get no event\n", __func__);
     }*/
-    LOGD("%s, [status=%d] [ret=%d]\n", __func__, *rds_status, ret);
+   // LOGD("%s, [status=%d] [ret=%d]\n", __func__, *rds_status, ret);
     return ret;
 }
 
@@ -815,9 +815,9 @@ int FMR_active_af(int idx, uint16_t *ret_freq)
                                     ret_freq);
     if ((ret == 0) && (*ret_freq != fmr_data.cur_freq)) {
         fmr_data.cur_freq = *ret_freq;
-        LOGI("active AF OK, new channel[freq=%d]\n", fmr_data.cur_freq);
+      //  LOGI("active AF OK, new channel[freq=%d]\n", fmr_data.cur_freq);
     }
-    LOGD("%s, [ret=%d]\n", __func__, ret);
+  //  LOGD("%s, [ret=%d]\n", __func__, ret);
     return ret;
 }
 
@@ -830,13 +830,13 @@ int FMR_ana_switch(int idx, int antenna)
     if (fmr_data.cfg_data.short_ana_sup == true) {
         ret = FMR_cbk_tbl(idx).ana_switch(FMR_fd(idx), antenna);
         if (ret) {
-            LOGE("%s failed, [ret=%d]\n", __func__, ret);
+          //  LOGE("%s failed, [ret=%d]\n", __func__, ret);
         }
     } else {
-        LOGW("FM antenna switch not support!\n");
+       // LOGW("FM antenna switch not support!\n");
         ret = -ERR_UNSUPT_SHORTANA;
     }
 
-    LOGD("%s, [ret=%d]\n", __func__, ret);
+   // LOGD("%s, [ret=%d]\n", __func__, ret);
     return ret;
 }
